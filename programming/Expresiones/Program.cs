f8aeeb6c1a7178073ce775fa7725a4b766aa2442
Carlos Javier <xcaimdev04@gmail.com>
2024-09-22 17:09:13 -0400

    First program

---
+++ programming/Expresiones/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace expresiones
+{
+    public class MyClass
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hola Mundo");
+        }
+    }
+}
