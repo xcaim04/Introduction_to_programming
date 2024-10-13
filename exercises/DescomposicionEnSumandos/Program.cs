67002ca6e64042f6201075c9b66138400507a43a
Carlos Javier Blanco <xcaimdev04@gmail.com>
2024-10-13 01:41:25 -0400

    Update exercises and TC as practice

---
+++ exercises/DescomposicionEnSumandos/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace MOG
+{
+    class Descomposition
+    {
+        public static int count = 1;
+
+        public static void AllSums(int number)
+        {
+            if (number == 0)
+                count ++;
+            else
+            {
+                for (int i=1;i<=(number+1)/2;i++)
+                    AllSums(number-i);
+            }
+        }
+
+        public static int Counter(int number)
+        {
+            AllSums(number);
+            return count;
+        }
+
+        public static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            System.Console.WriteLine(Counter(number));
+        }
+    }
+}
